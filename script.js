08de805913f525edd524149efa8369bbbefd5af0
mrgh0st9158 <athosclpimenta@gmail.com>
2026-06-06 18:30:18 -0300

    atualização site dia namorados

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
-const baly_video = document.getElementById("video_baly_miando")
+const baly_video_miando = document.getElementById("video_baly_miando")
+const baly_video_mordendo = document.getElementById("video_baly_mordendo")
 
 
 function voltar_inicio(){
     const todas_secoes = document.querySelectorAll("section")
 
     todas_secoes.forEach(function(secoes){
-        secoes.style.display = "none"
+        secoes.classList.add("tela_escondida")
 
     })
 
-    baly_video.pause();
+    baly_video_miando.pause();
+    baly_video_mordendo.pause();
 
-    document.getElementById("tela_inicial").style.display = "block"
+    document.getElementById("tela_inicial").classList.remove("tela_escondida")
 
     
 }
 
 
 function abrir_tela_dicas(){
-    document.getElementById("tela_inicial").style.display = "none"
+    document.getElementById("tela_inicial").classList.add("tela_escondida")
 
-    document.getElementById("tela_dicas").style.display = "block"
+    document.getElementById("tela_dicas").classList.remove("tela_escondida")
 
-    baly_video.play();
+    baly_video_miando.play();
 
 }
 
 function botao_presente(){
-    document.getElementById("tela_dicas").style.display = "none"
+    document.getElementById("tela_dicas").classList.add("tela_escondida")
     
-    document.getElementById("tela_dica_presentes").style.display = "block"
+    document.getElementById("tela_dica_presentes").classList.remove("tela_escondida")
 
-    baly_video.pause();
-    
-}
+    baly_video_miando.pause();
 
-function caixa_bombom_dica(){
-    document.getElementById("p-dica_caixa_bombom").innerHTML = 
-    ("Que tal surpreender seu parceiro ou parceira com uma linda caixa de bombons. Isso aí, você não precisa nem avisar nada. Pode ser uma surpresa para ele(a). Tenho certeza de que ele(a) vai adorar.  (Você ganha pontos extras se tomar banho antes da surpresa!)")
+    baly_video_mordendo.play();
 
-    document.getElementById("foto_gato_chocolate").style.display = "block"
-
-    baly_video.pause();
 }
