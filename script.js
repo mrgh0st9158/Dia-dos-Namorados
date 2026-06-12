1bdb4604c15efb6280530808a287a81352101f74
mrgh0st9158 <athosclpimenta@gmail.com>
2026-06-11 21:07:35 -0300

    atualização final site dia namorados

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 
-
-
 /* ==========================================================================
-   CÓDIGOS SITE
+   1. CÓDIGOS DO SITE
    ========================================================================== */
    
 
@@ -48,7 +46,7 @@ function botao_presente(){
 }
 
 /* ==========================================================================
-   SISTEMA DA ANIMAÇÃO CARROSEL
+   2. SISTEMA DA ANIMAÇÃO CARROSEL
    ========================================================================== */
 
 var shadow = '0 20px 50px rgba(0,34,45,0.5)';
@@ -73,23 +71,42 @@ function alternarFoco(botaoAtivo) {
 }
 
 // Eventos de clique para cada botão
+
+function alternar_texto(id_texto_bloco){
+    if(id_texto_bloco){
+        document.getElementById("p-buque").classList.add("tela_escondida")
+        document.getElementById("p-caixa_bombom").classList.add("tela_escondida")
+        document.getElementById("p-carta_a_mao").classList.add("tela_escondida")
+    }
+
+    document.getElementById(id_texto_bloco).classList.remove("tela_escondida")
+
+}
+
 document.getElementById('one').addEventListener('click', function() {
     alternarFoco('one');
-    styles('#first', 0, 0, 0, 1, shadow);
-    styles('#second', 70, -80, -50, 0.6, 'none');
-    styles('#third', 110, 80, -60, 0.1, 'none');
+    styles('#first', 120, -40, 0, 1, shadow);
+    styles('#second', 400, 0, -60, 0.6, 'none');
+    styles('#third', -100, 0, -50, 0.1, 'none');
+
+    alternar_texto("p-buque")
 });
 
 document.getElementById('two').addEventListener('click', function() {
     alternarFoco('two');
-    styles('#first', 110, 80, -60, 0.1, 'none');
-    styles('#second', 0, 0, 0, 1, shadow);
-    styles('#third', 70, -80, -50, 0.6, 'none');
+    styles('#first', 400, 0, -60, 0.1, 'none');
+    styles('#second', 120, -40, 0, 1, shadow);
+    styles('#third', -100, 0, -50, 0.6, 'none');
+
+    alternar_texto("p-caixa_bombom")
+    
 });
 
 document.getElementById('three').addEventListener('click', function() {
     alternarFoco('three');
-    styles('#first', 70, -80, -50, 0.6, 'none');
-    styles('#second', 110, 80, -60, 0.1, 'none');
-    styles('#third', 0, 0, 0, 1, shadow);
+    styles('#first', -100, 0, -50, 0.6, 'none');
+    styles('#second', 400, 0, -60, 0.1, 'none');
+    styles('#third', 120, -40, 0, 1, shadow);
+
+    alternar_texto("p-carta_a_mao")
 });
